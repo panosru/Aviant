87122965ac5a19c1cf8e4da1e6f41cb9bfe41ea5
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-26 02:32:48 +0300

    Refactor Entities

--- src/Domain/Entities/Entity.cs
+++ src/Domain/Entities/Entity.cs
@@ -1,10 +1,42 @@
 namespace Aviant.DDD.Domain.Entities
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
-    public abstract class Entity<T> : IEntity<T>
+    public abstract class Entity<TKey> : IEntity<TKey>
     {
-        public T Id { get; set; } = default!;
+        public TKey Id { get; protected set; }// = default!;
+        
+        protected Entity()
+        {}
+
+        protected Entity(TKey id) => Id = id;
+
+        #region Equality Check
+        
+        public override bool Equals(object? obj)
+        {
+            return obj is Entity<TKey> entity && GetType() == entity.GetType() &&
+                   EqualityComparer<TKey>.Default.Equals(Id, entity.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(GetType(), Id);
+        }
+
+        public static bool operator ==(Entity<TKey> left, Entity<TKey> right)
+        {
+            return EqualityComparer<Entity<TKey>>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Entity<TKey> left, Entity<TKey> right)
+        {
+            return !(left == right);
+        }
+        
+        #endregion
 
         public virtual Task<bool> Validate()
         {
--- src/Domain/Entities/IEntity.cs
+++ src/Domain/Entities/IEntity.cs
@@ -2,9 +2,9 @@ namespace Aviant.DDD.Domain.Entities
 {
     using System.Threading.Tasks;
 
-    public interface IEntity<T>
+    public interface IEntity<out TKey>
     {
-        public T Id { get; set; }
+        TKey Id { get; }
 
         Task<bool> Validate();
     }
