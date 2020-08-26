9d26b6def6330fb747098c614dc6bcccb65265bc
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-26 03:19:15 +0300

    Added RetryProcessor
    source: https://www.davideguida.com/mediatr-how-to-use-decorators-to-add-retry-policies/

---
+++ src/Application/Processors/RetryProcessor.cs
@@ -0,0 +1,30 @@
+namespace Aviant.DDD.Application.Processors
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+    using Polly;
+
+    public class RetryProcessor<TNotification> : INotificationHandler<TNotification>
+        where TNotification : INotification
+    {
+        private readonly INotificationHandler<TNotification> _inner;
+        private readonly IAsyncPolicy _retryPolicy;
+
+        public RetryProcessor(INotificationHandler<TNotification> inner)
+        {
+            _inner = inner; //TODO: check RetryDecorator doesn't get injected twice
+            _retryPolicy = Policy.Handle<ArgumentOutOfRangeException>()
+                .WaitAndRetryAsync(
+                    3,
+                    i => TimeSpan.FromSeconds(i));
+        }
+
+        public Task Handle(TNotification notification, CancellationToken cancellationToken)
+        {
+            return _retryPolicy.ExecuteAsync(() => 
+                _inner.Handle(notification, cancellationToken));
+        }
+    }
+}
