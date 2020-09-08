347fe31904f03ad6309bec29b14ce01983bf1c97
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-08 13:21:07 +0300

    UnitOfWork moved from Domain layer to Application layer

---
+++ src/Application/Persistance/IUnitOfWork.cs
@@ -0,0 +1,31 @@
+namespace Aviant.DDD.Application.Persistance
+{
+    using System;
+    using System.Threading.Tasks;
+    using Domain.Aggregates;
+
+    /// <summary>
+    ///     Unit of Work Interface
+    /// </summary>
+    public interface IUnitOfWork<TDbContext>
+        where TDbContext : IApplicationDbContext
+    {
+        /// <summary>
+        ///     Commit changes to database persistence
+        /// </summary>
+        /// <returns>Integer representing affected rows</returns>
+        Task<int> Commit();
+    }
+
+    public interface IUnitOfWork<in TAggregateRoot, TAggregateId>
+        where TAggregateRoot : class, IAggregateRoot<TAggregateId>
+        where TAggregateId : class, IAggregateId
+    {
+        /// <summary>
+        /// Commit changes to event sourcing persistence
+        /// </summary>
+        /// <param name="aggregateRoot"></param>
+        /// <returns></returns>
+        Task<bool> Commit(TAggregateRoot aggregateRoot);
+    }
+}
