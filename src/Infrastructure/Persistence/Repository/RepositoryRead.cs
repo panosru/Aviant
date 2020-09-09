fff56440cfd90f35eebe11aa673dce4b2e3386c7
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-10 00:59:18 +0300

    Repository* work for both with and without Identity Db Context (unfortunately there is code duplication…)

--- src/Infrastructure/Persistence/Repository/RepositoryRead.cs
+++ src/Infrastructure/Persistence/Repository/RepositoryRead.cs
@@ -11,6 +11,150 @@ namespace Aviant.DDD.Infrastructure.Persistence.Repository
     using Domain.Persistence;
     using Microsoft.EntityFrameworkCore;
 
+    public abstract class RepositoryRead<TDbContext, TEntity, TPrimaryKey>
+        : IRepositoryRead<TEntity, TPrimaryKey>
+        where TEntity : Entity<TPrimaryKey>
+        where TDbContext : DbContextRead
+    {
+        private readonly TDbContext _dbContext;
+
+        private readonly DbSet<TEntity> _dbSet;
+
+        protected RepositoryRead(TDbContext context)
+        {
+            _dbContext = context;
+            _dbSet     = _dbContext.Set<TEntity>();
+        }
+
+        #region IRepositoryRead<TEntity,TPrimaryKey> Members
+
+        public IQueryable<TEntity> GetAll()
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            return query;
+        }
+
+        public IQueryable<TEntity> GetAllIncluding(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            BindIncludeProperties(query, includeProperties);
+            includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query;
+        }
+
+        public async Task<List<TEntity>> GetAllList() => await GetAll().ToListAsync();
+
+        public Task<List<TEntity>> GetAllListIncluding(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query.ToListAsync();
+        }
+
+        public ValueTask<TEntity> Find(TPrimaryKey id) => _dbSet.FindAsync(id);
+
+        public Task<TEntity> GetFirst(TPrimaryKey id) =>
+            GetAll().FirstOrDefaultAsync(CreateEqualityExpressionForId(id));
+
+        public Task<TEntity> GetFirstIncluding(
+            TPrimaryKey                                id,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query.FirstOrDefaultAsync(CreateEqualityExpressionForId(id));
+        }
+
+        public Task<TEntity> GetFirst(Expression<Func<TEntity, bool>> predicate) =>
+            GetAll().FirstOrDefaultAsync(predicate);
+
+        public Task<TEntity> GetFirstIncluding(
+            Expression<Func<TEntity, bool>>            predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query.FirstOrDefaultAsync(predicate);
+        }
+
+        public Task<TEntity> GetSingle(TPrimaryKey id) =>
+            GetAll().SingleOrDefaultAsync(CreateEqualityExpressionForId(id));
+
+        public Task<TEntity> GetSingleIncluding(
+            TPrimaryKey                                id,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query.SingleOrDefaultAsync(CreateEqualityExpressionForId(id));
+        }
+
+        public Task<TEntity> GetSingle(Expression<Func<TEntity, bool>> predicate) =>
+            GetAll().SingleOrDefaultAsync(predicate);
+
+        public Task<TEntity> GetSingleIncluding(
+            Expression<Func<TEntity, bool>>            predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query.SingleOrDefaultAsync(predicate);
+        }
+
+        public IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate) => GetAll().Where(predicate);
+
+        public IQueryable<TEntity> FindByIncluding(
+            Expression<Func<TEntity, bool>>            predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = GetAll();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return query.Where(predicate);
+        }
+
+        public Task<bool> Any(Expression<Func<TEntity, bool>> predicate) => _dbSet.AnyAsync(predicate);
+
+        public Task<bool> All(Expression<Func<TEntity, bool>> predicate) => _dbSet.AllAsync(predicate);
+
+        public async Task<int> Count() => await _dbSet.CountAsync();
+
+        public Task<int> Count(Expression<Func<TEntity, bool>> predicate) => _dbSet.CountAsync(predicate);
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        #endregion
+
+        private static void BindIncludeProperties(
+            IQueryable<TEntity>                            query,
+            IEnumerable<Expression<Func<TEntity, object>>> includeProperties)
+        {
+            includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        private static Expression<Func<TEntity, bool>> CreateEqualityExpressionForId(TPrimaryKey id)
+        {
+            var lambdaParam = Expression.Parameter(typeof(TEntity));
+
+            var lambdaBody = Expression.Equal(
+                Expression.PropertyOrField(lambdaParam, "Id"),
+                Expression.Constant(id, typeof(TPrimaryKey))
+            );
+
+            return Expression.Lambda<Func<TEntity, bool>>(lambdaBody, lambdaParam);
+        }
+    }
+    
     public abstract class RepositoryRead<TDbContext, TApplicationUser, TApplicationRole, TEntity, TPrimaryKey>
         : IRepositoryRead<TEntity, TPrimaryKey>
         where TEntity : Entity<TPrimaryKey>
--- src/Infrastructure/Persistence/Repository/RepositoryWrite.cs
+++ src/Infrastructure/Persistence/Repository/RepositoryWrite.cs
@@ -11,12 +11,10 @@ namespace Aviant.DDD.Infrastructure.Persistence.Repository
     using Domain.Persistence;
     using Microsoft.EntityFrameworkCore;
 
-    public abstract class RepositoryWrite<TDbContext, TApplicationUser, TApplicationRole, TEntity, TPrimaryKey>
+    public abstract class RepositoryWrite<TDbContext, TEntity, TPrimaryKey>
         : IRepositoryWrite<TEntity, TPrimaryKey>
         where TEntity : Entity<TPrimaryKey>
-        where TApplicationUser : ApplicationUser
-        where TApplicationRole : ApplicationRole
-        where TDbContext : AuthorizationDbContextWrite<TDbContext, TApplicationUser, TApplicationRole>
+        where TDbContext : DbContextWrite<TDbContext>
     {
         private readonly TDbContext _dbContext;
 
@@ -72,6 +70,77 @@ namespace Aviant.DDD.Infrastructure.Persistence.Repository
             _dbContext.Dispose();
         }
 
+        #endregion
+    }
+    
+    /**
+     * No one likes code duplication, but I can’t think of an elegant solution to use
+     * C# 8.0 default interface implementation for a trait-like pattern to avoid code
+     * duplication in this case (as I did for DbContext objects).
+     * TODO: Find a way to avoid code duplication here
+     */
+    
+    public abstract class RepositoryWrite<TDbContext, TApplicationUser, TApplicationRole, TEntity, TPrimaryKey>
+        : IRepositoryWrite<TEntity, TPrimaryKey>
+        where TEntity : Entity<TPrimaryKey>
+        where TApplicationUser : ApplicationUser
+        where TApplicationRole : ApplicationRole
+        where TDbContext : AuthorizationDbContextWrite<TDbContext, TApplicationUser, TApplicationRole>
+    {
+        private readonly TDbContext _dbContext;
+    
+        private readonly DbSet<TEntity> _dbSet;
+    
+        protected RepositoryWrite(TDbContext context)
+        {
+            _dbContext = context;
+            _dbSet     = _dbContext.Set<TEntity>();
+        }
+    
+        #region IRepositoryWrite<TEntity,TPrimaryKey> Members
+    
+        public async Task Add(TEntity entity)
+        {
+            // First validate entity's rules
+            await entity.Validate();
+    
+            await _dbSet.AddAsync(entity);
+        }
+    
+        public Task Update(TEntity entity)
+        {
+            // First validate entity's rules
+            entity.Validate();
+    
+            _dbContext.Entry(entity).State = EntityState.Modified;
+    
+            return Task.CompletedTask;
+        }
+    
+        public Task Delete(TEntity entity)
+        {
+            // First validate entity's rules
+            entity.Validate();
+    
+            _dbContext.Entry(entity).State = EntityState.Deleted;
+    
+            return Task.CompletedTask;
+        }
+    
+        public Task DeleteWhere(Expression<Func<TEntity, bool>> predicate)
+        {
+            IEnumerable<TEntity> entities = _dbSet.Where(predicate);
+    
+            foreach (var entity in entities) _dbContext.Entry(entity).State = EntityState.Deleted;
+    
+            return Task.CompletedTask;
+        }
+    
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    
         #endregion
     }
 }
