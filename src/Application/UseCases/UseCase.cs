929893bdb21a0c3a99e96b7ed751a8fccf116bed
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-18 21:37:26 +0300

    UseCase supports DbContextWrite and Aggregates

--- src/Application/UseCases/UseCase.cs
+++ src/Application/UseCases/UseCase.cs
@@ -3,9 +3,13 @@
 #pragma warning disable 8618
 namespace Aviant.DDD.Application.UseCases
 {
+    using System;
+    using System.Data;
     using System.Threading.Tasks;
+    using Core.Aggregates;
     using Core.Services;
     using Orchestration;
+    using Persistance;
 
     public abstract class UseCase<TUseCaseOutput> : IUseCase
         where TUseCaseOutput : class, IUseCaseOutput
@@ -42,6 +46,41 @@ namespace Aviant.DDD.Application.UseCases
             return Execute();
         }
 
+        protected virtual T GetDataByType<T>(dynamic? data)
+        {
+            if (data is null)
+                throw new ArgumentNullException(nameof(data));
+            
+            if (!(data is T dataType))
+                throw new TypeAccessException(
+                    $"Expected type \"{typeof(T).Name}\", but \"{data.GetType().Name}\" found instead.");
+
+            return (T) dataType;
+        }
+        
         protected abstract void SetInput<TInputData>(TInputData data);
+    }
+    
+    public abstract class UseCase<TUseCaseInput, TUseCaseOutput, TDbContext> 
+        : UseCase<TUseCaseInput, TUseCaseOutput>
+        where TUseCaseInput : class, IUseCaseInput
+        where TUseCaseOutput : class, IUseCaseOutput
+        where TDbContext : IDbContextWrite
+    {
+        protected new IOrchestrator<TDbContext> Orchestrator =>
+            ServiceLocator.ServiceContainer.GetRequiredService<IOrchestrator<TDbContext>>(
+                typeof(IOrchestrator<TDbContext>));
+    }
+    
+    public abstract class UseCase<TUseCaseInput, TUseCaseOutput, TAggregate, TAggregateId> 
+        : UseCase<TUseCaseInput, TUseCaseOutput>
+        where TUseCaseInput : class, IUseCaseInput
+        where TUseCaseOutput : class, IUseCaseOutput
+        where TAggregate : class, IAggregate<TAggregateId>
+        where TAggregateId : class, IAggregateId
+    {
+        protected new IOrchestrator<TAggregate, TAggregateId> Orchestrator =>
+            ServiceLocator.ServiceContainer.GetRequiredService<IOrchestrator<TAggregate, TAggregateId>>(
+                typeof(IOrchestrator<TAggregate, TAggregateId>));
     }
 }
