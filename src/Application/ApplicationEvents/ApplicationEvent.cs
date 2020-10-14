977cdc14bb395109e8752df21e6cdf18c7bd8199
Panagiotis <400362+panosru@users.noreply.github.com>
2020-10-14 18:32:16 +0300

    Application Events get Occurred property populated uppon creation

--- src/Application/ApplicationEvents/ApplicationEvent.cs
+++ src/Application/ApplicationEvents/ApplicationEvent.cs
@@ -1,12 +1,19 @@
 namespace Aviant.DDD.Application.ApplicationEvents
 {
     using System;
+    using Core.Services;
+    using Services;
 
     public abstract class ApplicationEvent : IApplicationEvent
     {
         #region IApplicationEvent Members
 
         public DateTime Occured { get; set; }
+
+        protected ApplicationEvent() =>
+            Occured = ServiceLocator.ServiceContainer.GetService<IDateTimeService>(
+                    typeof(IDateTimeService))
+               .Now(true);
 
         #endregion
     }
