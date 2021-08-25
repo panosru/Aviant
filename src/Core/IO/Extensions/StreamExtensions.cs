716ebd0ab3cc8cce1e15d1e91c01ed0a4208e9dc
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-25 18:10:39 +0300

    IO helpers & extensions (from abp)

---
+++ src/Core/IO/Extensions/StreamExtensions.cs
@@ -0,0 +1,15 @@
+namespace Aviant.DDD.Core.IO.Extensions
+{
+    using System.IO;
+
+    public static class StreamExtensions
+    {
+        public static byte[] GetAllBytes(this Stream stream)
+        {
+            using var memoryStream = new MemoryStream();
+
+            stream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
