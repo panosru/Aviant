54c42a1fba60361b35577d5332461abb1956a38c
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-25 16:58:16 +0300

    Added Domain Configuration Container

---
+++ src/Core/Domain/IDomainConfigurationContainer.cs
@@ -0,0 +1,11 @@
+namespace Aviant.DDD.Core.Domain
+{
+    using Microsoft.Extensions.Configuration;
+
+    public interface IDomainConfigurationContainer
+    {
+        public IConfiguration Configuration();
+
+        public string GetValue(string path);
+    }
+}
---
+++ src/Infrastructure/CrossCutting/DomainConfigurationContainer.cs
@@ -0,0 +1,16 @@
+namespace Aviant.DDD.Infrastructure.CrossCutting
+{
+    using Core.Domain;
+    using Microsoft.Extensions.Configuration;
+
+    public class DomainConfigurationContainer : IDomainConfigurationContainer
+    {
+        private readonly IConfiguration _configuration;
+
+        protected DomainConfigurationContainer(IConfiguration configuration) => _configuration = configuration;
+
+        public IConfiguration Configuration() => _configuration;
+
+        public string GetValue(string path) => _configuration[path];
+    }
+}
