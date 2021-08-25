ba68b2b0329f77f25e4aebeabf36825423c8c68a
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-25 17:13:13 +0300

    JSON extension

---
+++ src/Core/Json/DateTimeConverter.cs
@@ -0,0 +1,44 @@
+namespace Aviant.DDD.Core.Json
+{
+    using System;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Timing;
+
+    public sealed class DateTimeConverter : IsoDateTimeConverter
+    {
+        public override bool CanConvert(Type objectType) =>
+            objectType == typeof(DateTime) || objectType == typeof(DateTime?);
+
+        public override object? ReadJson(
+            JsonReader     reader,
+            Type           objectType,
+            object?        existingValue,
+            JsonSerializer serializer)
+        {
+            if (base.ReadJson(
+                reader,
+                objectType,
+                existingValue,
+                serializer) is DateTime date)
+                return Clock.Normalize(date);
+
+            return null;
+        }
+
+        public override void WriteJson(
+            JsonWriter     writer,
+            object?        value,
+            JsonSerializer serializer)
+        {
+            var date = value as DateTime?;
+
+            base.WriteJson(
+                writer,
+                date.HasValue
+                    ? Clock.Normalize(date.Value)
+                    : value,
+                serializer);
+        }
+    }
+}
