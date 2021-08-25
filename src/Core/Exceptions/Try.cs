997552cf3a73134d6a64286397e4abaa61ca75a7
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-25 18:06:08 +0300

    Custom Try Exception manage (from stove project)

---
+++ src/Core/Exceptions/Try.cs
@@ -0,0 +1,47 @@
+namespace Aviant.DDD.Core.Exceptions
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class Try
+    {
+        public static void Silently(
+            Action            callback,
+            Action<Exception> onException,
+            Action?           finallyCallback = null)
+        {
+            try
+            {
+                callback();
+            }
+            catch (Exception exception)
+            {
+                onException(exception);
+            }
+            finally
+            {
+                finallyCallback?.Invoke();
+            }
+        }
+
+        public static async Task SilentlyAsync(
+            Func<Task>            callback,
+            Func<Exception, Task> onException,
+            Func<Task>?           finallyCallback = null)
+        {
+            try
+            {
+                await callback();
+            }
+            catch (Exception ex)
+            {
+                await onException(ex);
+            }
+            finally
+            {
+                if (finallyCallback != null)
+                    await finallyCallback();
+            }
+        }
+    }
+}
