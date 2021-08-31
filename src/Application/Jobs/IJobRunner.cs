2e156b60401e091fe874ae4b6a0161fafccf6eeb
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-31 11:43:36 +0300

    Background jobs with support of Hangfire

---
+++ src/Application/Jobs/IJobRunner.cs
@@ -0,0 +1,46 @@
+namespace Aviant.DDD.Application.Jobs
+{
+    using System;
+    using System.Threading.Tasks;
+    using Hangfire.States;
+
+    public interface IJob<in TJobOptions>
+        where TJobOptions : class, IJobOptions
+    {
+        Task Perform(TJobOptions jobOptions);
+    }
+
+    public interface IJobRunner
+    {
+        string Run<TJob, TJobOptions>(Action<TJobOptions>? configureJobOptions = null)
+            where TJobOptions : class, IJobOptions
+            where TJob : IJob<TJobOptions>;
+
+        string RunInState<TJob, TJobOptions>(IState state, Action<TJobOptions>? configureJobOptions = null)
+            where TJobOptions : class, IJobOptions
+            where TJob : IJob<TJobOptions>;
+
+        string RunWithDelay<TJob, TJobOptions>(TimeSpan delay, Action<TJobOptions>? configureJobOptions = null)
+            where TJobOptions : class, IJobOptions
+            where TJob : IJob<TJobOptions>;
+
+        string RunAtDateTime<TJob, TJobOptions>(DateTime dateTime, Action<TJobOptions>? configureJobOptions = null)
+            where TJobOptions : class, IJobOptions
+            where TJob : IJob<TJobOptions>;
+
+        string RunAfter<TJob, TJobOptions>(string previousJobId, Action<TJobOptions>? configureJobOptions = null)
+            where TJobOptions : class, IJobOptions
+            where TJob : IJob<TJobOptions>;
+
+        string RunRecurring<TJob, TJobOptions>(
+            string               jobId,
+            string               cron,
+            Action<TJobOptions>? configureJobOptions = null)
+            where TJobOptions : class, IJobOptions
+            where TJob : IJob<TJobOptions>;
+
+        void TriggerRecurringJob(string id);
+
+        void RemoveRecurringJob(string id);
+    }
+}
---
+++ src/Application/Jobs/JobRunner.cs
@@ -0,0 +1,110 @@
+namespace Aviant.DDD.Application.Jobs
+{
+    using System;
+    using Core.Timing;
+    using Hangfire;
+    using Hangfire.States;
+
+    public interface IJobOptions
+    { }
+
+    public class JobRunner : IJobRunner
+    {
+        public JobRunner(IBackgroundJobClient backgroundJobClient, IRecurringJobManager recurringJobManager)
+        {
+            BackgroundJobClient = backgroundJobClient;
+            RecurringJobManager = recurringJobManager;
+        }
+
+        private IBackgroundJobClient BackgroundJobClient { get; }
+
+        private IRecurringJobManager RecurringJobManager { get; }
+
+        #region IJobRunner Members
+
+        /// <inheritdoc />
+        public string Run<TJob, TJobOptions>(Action<TJobOptions>? configureJobOptions)
+            where TJob : IJob<TJobOptions>
+            where TJobOptions : class, IJobOptions
+        {
+            return BackgroundJobClient.Enqueue<TJob>(job => job.Perform(BuildOptions(configureJobOptions)));
+        }
+
+        /// <inheritdoc />
+        public string RunInState<TJob, TJobOptions>(IState state, Action<TJobOptions>? configureJobOptions)
+            where TJob : IJob<TJobOptions>
+            where TJobOptions : class, IJobOptions
+        {
+            return BackgroundJobClient.Create<TJob>(
+                job =>
+                    job.Perform(BuildOptions(configureJobOptions)),
+                state);
+        }
+
+        /// <inheritdoc />
+        public string RunWithDelay<TJob, TJobOptions>(TimeSpan delay, Action<TJobOptions>? configureJobOptions)
+            where TJob : IJob<TJobOptions>
+            where TJobOptions : class, IJobOptions
+        {
+            return BackgroundJobClient.Schedule<TJob>(
+                job =>
+                    job.Perform(BuildOptions(configureJobOptions)),
+                Clock.Now + delay);
+        }
+
+        /// <inheritdoc />
+        public string RunAtDateTime<TJob, TJobOptions>(DateTime dateTime, Action<TJobOptions>? configureJobOptions)
+            where TJob : IJob<TJobOptions>
+            where TJobOptions : class, IJobOptions
+        {
+            return BackgroundJobClient.Schedule<TJob>(
+                job =>
+                    job.Perform(BuildOptions(configureJobOptions)),
+                dateTime);
+        }
+
+        /// <inheritdoc />
+        public string RunAfter<TJob, TJobOptions>(string previousJobId, Action<TJobOptions>? configureJobOptions)
+            where TJob : IJob<TJobOptions>
+            where TJobOptions : class, IJobOptions
+        {
+            return BackgroundJobClient.ContinueJobWith<TJob>(
+                previousJobId,
+                job => job.Perform(BuildOptions(configureJobOptions)));
+        }
+
+        /// <inheritdoc />
+        public string RunRecurring<TJob, TJobOptions>(
+            string               jobId,
+            string               cron,
+            Action<TJobOptions>? configureJobOptions)
+            where TJob : IJob<TJobOptions>
+            where TJobOptions : class, IJobOptions
+        {
+            RecurringJobManager.AddOrUpdate<TJob>(
+                jobId,
+                job => job.Perform(BuildOptions(configureJobOptions)),
+                cron);
+
+            return jobId;
+        }
+
+        /// <inheritdoc />
+        public void TriggerRecurringJob(string id) => RecurringJobManager.Trigger(id);
+
+        /// <inheritdoc />
+        public void RemoveRecurringJob(string id) => RecurringJobManager.RemoveIfExists(id);
+
+        #endregion
+
+        private static TJobOptions BuildOptions<TJobOptions>(Action<TJobOptions>? configureJobOptions)
+            where TJobOptions : class, IJobOptions
+        {
+            var jobOptions = Activator.CreateInstance<TJobOptions>();
+
+            configureJobOptions?.Invoke(jobOptions);
+
+            return jobOptions;
+        }
+    }
+}
