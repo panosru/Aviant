1c730e35db563a82e76a8bc91159178828999328
Panagiotis <400362+panosru@users.noreply.github.com>
2022-08-05 01:26:32 +0300

    Add AspectBaseAttribute

---
+++ src/Kernel/Core/Aspects/AspectBaseAttribute.cs
@@ -0,0 +1,32 @@
+namespace Aviant.Core.Aspects;
+
+using AspectCore.DynamicProxy;
+
+public abstract class AspectBaseAttribute : AbstractInterceptorAttribute
+{
+    /// <inheritdoc />
+    public override async Task Invoke(AspectContext context, AspectDelegate next)
+    {
+        try
+        {
+            Pre(context);
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            Error(exception);
+        }
+        finally
+        {
+            Post(context);
+        }
+    }
+
+    protected virtual void Pre(AspectContext context)
+    { }
+
+    protected virtual void Post(AspectContext context)
+    { }
+
+    protected virtual void Error(Exception exception) => throw exception;
+}
