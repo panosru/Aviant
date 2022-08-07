d42d78b03699faa933ed231118008f3b0a336fbe
Panagiotis <400362+panosru@users.noreply.github.com>
2022-08-08 00:16:42 +0300

    Added LastAccessed property to ApplicationUser

--- src/Identity/Application/ApplicationUser.cs
+++ src/Identity/Application/ApplicationUser.cs
@@ -3,4 +3,6 @@ namespace Aviant.Application.Identity;
 using Microsoft.AspNetCore.Identity;
 
 public abstract class ApplicationUser : IdentityUser<Guid>
-{ }
+{
+    public DateTime LastAccessed { get; set; }
+}
